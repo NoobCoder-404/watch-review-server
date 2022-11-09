71912f524c2e7708370437a09229a2aaba681ed4
Noob-H4cker <alsamiul123@gmail.com>
2022-11-09 14:42:17 +0600

    services details api created

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 
 const app = express();
@@ -17,20 +17,29 @@ app.use(express.json());
 // console.log(process.env.DB_PASSWORD);
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.rqwof3p.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
+//console.log(uri);
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run(){
     try{
-        const productCollection = client.db('watchReview').collection('products');
+        const serviceCollection = client.db('watchReview').collection('products');
 
         app.get('/services',async(req,res) => {
         const query ={};
-        const cursor = productCollection.find(query);
-        const products = await cursor.toArray();
-        res.send(products);
-        })
+        const cursor = serviceCollection.find(query);
+        const services = await cursor.toArray();
+        res.send(services);
+        });
+
+       app.get('/services/:id', async(req,res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+       });
+
+
         
     }
     finally{
